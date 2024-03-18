eafe0ec4547c8df64787c6e36e9182aa13e08d0d
phamminh2005 <23020118@vnu.edu.vn>
2024-03-18 22:09:13 +0700

    Update

--- AdvProg_L6-Painter/painter.cpp
+++ AdvProg_L6-Painter/painter.cpp
@@ -9,6 +9,8 @@
 void Painter::setColor(SDL_Color color) 
 { 
     // TODO: set the color value for the Painter and set Render Draw Color
+    SDL_Renderer* renderer;
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b,color.a);
 }
 
 
@@ -21,6 +23,9 @@ void Painter::setColor(SDL_Color color)
 void Painter::jumpForward(int numPixel)
 {
     // TODO: jump the painter forward
+    
+    x += numPixel;
+    y += numPixel;
 }
 
 
@@ -33,6 +38,8 @@ void Painter::jumpForward(int numPixel)
 void Painter::jumpBackward(int numPixel)
 {
     // TODO: jump the painter backward
+    x -= numPixel;
+    y -= numPixel;
 }
 
 
@@ -45,6 +52,7 @@ void Painter::jumpBackward(int numPixel)
 void Painter::turnLeft(double degree)
 {
     // TODO: rotate left the painter   
+    angle -= degree;
 }
 
 
@@ -57,6 +65,7 @@ void Painter::turnLeft(double degree)
 void Painter::turnRight(double degree)
 {
     // TODO: rotate right the painter   
+    angle += degree;
 }
 
 /***  
@@ -67,7 +76,15 @@ void Painter::turnRight(double degree)
 ***/
 void Painter::randomColor()
 {
+    srand(time(NULL));
     // TODO: set random color    
+    int r = rand() % 256;
+    int g = rand() % 256;
+    int b = rand() % 256;
+    int a = rand() % 256;
+    SDL_Color randomColor = { r,g,b,a };
+    setColor(randomColor);
+
 }
 
 
